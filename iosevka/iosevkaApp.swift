6ff6c2e4c52e71e1ae59f02c4ca53d389fed63c9
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-10-05 14:59:09 -0700

    Initial Commit

---
+++ iosevka/iosevkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosevkaApp.swift
+//  iosevka
+//
+//  Created by Zaymon Foulds-Cook on 5/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iosevkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
