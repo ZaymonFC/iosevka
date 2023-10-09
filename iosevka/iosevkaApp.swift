4ae685d6f033dbb6d6d6c4a5f141f62e3fe8ef29
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2023-10-09 12:06:37 -0700

    💅 Visual: FadeIn

--- iosevka/iosevkaApp.swift
+++ iosevka/iosevkaApp.swift
@@ -28,6 +28,26 @@ struct AppView: View {
 
 struct AppView_Previews: PreviewProvider {
   static var previews: some View {
-    AppView()
+    AppView().fadeIn()
+  }
+}
+
+struct FadeIn: ViewModifier {
+  @State private var opacity: Double = 0
+
+  func body(content: Content) -> some View {
+    content
+      .opacity(opacity)
+      .onAppear {
+        withAnimation(.easeIn(duration: 0.2)) {
+          opacity = 1
+        }
+      }
+  }
+}
+
+extension View {
+  func fadeIn() -> some View {
+    modifier(FadeIn())
   }
 }
